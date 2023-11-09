05c4949997d6a4250f47bdfe0d2eedab59a16493
Gyeongho Yang <chchch1213@naver.com>
2023-11-09 18:36:37 +0900

    feat(controller): Application 진입점에서 XmasController 실행

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,9 @@
 package christmas;
 
+import christmas.controller.XmasController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        XmasController.run();
     }
 }
