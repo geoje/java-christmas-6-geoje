836fb347a07b4882d6432ea9da780cb30fe9d8ea
Gyeongho Yang <chchch1213@naver.com>
2023-11-09 18:33:35 +0900

    feat(const): 출력을 담당할 OutputView Class 생성

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,15 @@
+package christmas.view;
+
+import christmas.constant.GeneralMessage;
+
+import static christmas.constant.GeneralMessage.NOTIFY_INTRODUCTION;
+
+public class OutputView {
+    private static void printGeneralMessage(GeneralMessage message, Object... args) {
+        System.out.printf(message.toString() + System.lineSeparator(), args);
+    }
+
+    public static void printIntroduction() {
+        printGeneralMessage(NOTIFY_INTRODUCTION);
+    }
+}
