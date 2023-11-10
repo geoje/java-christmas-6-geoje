02e7fef1c25fa8b326310b363d6cb40d07a6e59b
Gyeongho Yang <chchch1213@naver.com>
2023-11-10 20:05:39 +0900

    feat(const): 알림 및 질문 메세지 상수 추가

--- src/main/java/christmas/constant/GeneralMessage.java
+++ src/main/java/christmas/constant/GeneralMessage.java
@@ -1,8 +1,19 @@
 package christmas.constant;
 
 public enum GeneralMessage {
-    NOTIFY_INTRODUCTION("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
-    ASK_VISITING_DAY("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    NOTIFY_INTRODUCTION("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다."),
+    NOTIFY_PREVIEW("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+
+    ASK_VISITING_DAY("%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    ASK_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+
+    TITLE_ORDER_MENU("<주문 메뉴>"),
+    TITLE_AMOUNT_BEFORE_DISCOUNT("<할인 전 총주문 금액>"),
+    TITLE_GIFT_MENU("<증정 메뉴>"),
+    TITLE_BENEFIT_DETAILS("<혜택 내역>"),
+    TITLE_TOTAL_BENEFIT_AMOUNT("<총혜택 금액>"),
+    TITLE_AMOUNT_AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
+    TITLE_EVENT_BADGE("<%d월 이벤트 배지>");
 
     private final String message;
 
