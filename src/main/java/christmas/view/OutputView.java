5b8cc2e224cc89a4b6c922a74900af8b4839defd
Gyeongho Yang <chchch1213@naver.com>
2023-11-10 21:48:17 +0900

    feat(view): 에러 메세지 및 헤택 미리보기 메세지 출력 함수 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,14 +2,24 @@ package christmas.view;
 
 import christmas.constant.GeneralMessage;
 
+import static christmas.constant.ErrorMessage.EXCEPTION_PREFIX;
 import static christmas.constant.GeneralMessage.NOTIFY_INTRODUCTION;
+import static christmas.constant.GeneralMessage.NOTIFY_PREVIEW;
 
 public class OutputView {
     private static void printGeneralMessage(GeneralMessage message, Object... args) {
-        System.out.printf(message.toString() + System.lineSeparator(), args);
+        System.out.printf(message + System.lineSeparator(), args);
+    }
+
+    private static void printErrorMessage(String message) {
+        System.out.println(EXCEPTION_PREFIX + message);
     }
 
     public static void printIntroduction(int month) {
         printGeneralMessage(NOTIFY_INTRODUCTION, month);
+    }
+
+    public static void printPreview(int month, int day) {
+        printGeneralMessage(NOTIFY_PREVIEW, month, day);
     }
 }
