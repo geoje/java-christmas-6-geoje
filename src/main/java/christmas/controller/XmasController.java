ec9a94bc0a9567b2052fb369c1c47d79367ffc8b
Gyeongho Yang <chchch1213@naver.com>
2023-11-10 21:05:18 +0900

    feat(controller): 입출력에 달이 출력 되도록 인자 전달

--- src/main/java/christmas/controller/XmasController.java
+++ src/main/java/christmas/controller/XmasController.java
@@ -3,9 +3,11 @@ package christmas.controller;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import static christmas.constant.PromotionCalendar.PERIOD_MONTH;
+
 public class XmasController {
     public static void run() {
-        OutputView.printIntroduction();
-        InputView.askVisitingDay();
+        OutputView.printIntroduction(PERIOD_MONTH.getRealMonth());
+        InputView.askVisitingDay(PERIOD_MONTH.getRealMonth());
     }
 }
