9f0609d7bdca15c174fc932daf05d29a4db35d49
Gyeongho Yang <chchch1213@naver.com>
2023-11-10 21:03:03 +0900

    feat(view): InputView에서도 질문메세지에 format된 메세지 출력 가능하도록 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,12 +6,12 @@ import christmas.constant.GeneralMessage;
 import static christmas.constant.GeneralMessage.ASK_VISITING_DAY;
 
 public class InputView {
-    private static String readWithGeneralMessage(GeneralMessage message) {
-        System.out.println(message.toString());
+    private static String readWithGeneralMessage(GeneralMessage message, Object... args) {
+        System.out.printf(message.toString() + System.lineSeparator(), args);
         return Console.readLine().trim();
     }
 
-    public static String askVisitingDay() {
-        return readWithGeneralMessage(ASK_VISITING_DAY);
+    public static String askVisitingDay(int month) {
+        return readWithGeneralMessage(ASK_VISITING_DAY, month);
     }
 }
