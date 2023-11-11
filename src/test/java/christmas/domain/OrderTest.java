d05b3b9d3d6b1bae65289c61b6981c5e265f59c9
Gyeongho Yang <chchch1213@naver.com>
2023-11-11 21:45:24 +0900

    refactor(domain): 메뉴 인자 변수명 복수형으로 변경

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -12,9 +12,9 @@ public class OrderTest {
     @ParameterizedTest
     @DisplayName("from 함수를 통한 생성")
     @ValueSource(strings = {"해산물파스타-2,레드와인-1,초코케이크-1"})
-    void fromInstance(String menu) {
+    void fromInstance(String menus) {
         assertThatCode(() -> {
-            Order order = Order.from(menu);
+            Order order = Order.from(menus);
             System.out.println(order);
         }).doesNotThrowAnyException();
     }
@@ -22,15 +22,15 @@ public class OrderTest {
     @ParameterizedTest
     @DisplayName("없는 메뉴 제거 후 어느정도 유연한 생성")
     @ValueSource(strings = {" , ,,해산물파스타-2,레드와인-1, ,,"})
-    void fromFlexible(String menu) {
-        assertThatCode(() -> Order.from(menu)).doesNotThrowAnyException();
+    void fromFlexible(String menus) {
+        assertThatCode(() -> Order.from(menus)).doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @DisplayName("메뉴 이름과 개수 사이에 분리자가 없을 경우 에외 발생")
     @ValueSource(strings = {"해산물파스타-2,레드와인1", "해산물파스타,레드와인"})
-    void validateNameCountDelimiter(String menu) {
-        assertThatCode(() -> Order.from(menu))
+    void validateNameCountDelimiter(String menus) {
+        assertThatCode(() -> Order.from(menus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ORDER_INVALID.toString());
     }
@@ -38,8 +38,8 @@ public class OrderTest {
     @ParameterizedTest
     @DisplayName("메뉴 개수가 없거나 숫자가 아닐 경우 예외 발생")
     @ValueSource(strings = {"해산물파스타-,레드와인-1", "해산물파스타-둘,레드와인-1"})
-    void validateNumeric(String menu) {
-        assertThatCode(() -> Order.from(menu))
+    void validateNumeric(String menus) {
+        assertThatCode(() -> Order.from(menus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ORDER_INVALID.toString());
     }
@@ -47,8 +47,8 @@ public class OrderTest {
     @ParameterizedTest
     @DisplayName("메뉴 개수가 0 일 경우 예외 발생")
     @ValueSource(strings = {"해산물파스타-0,레드와인-1"})
-    void validateCount(String menu) {
-        assertThatCode(() -> Order.from(menu))
+    void validateCount(String menus) {
+        assertThatCode(() -> Order.from(menus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ORDER_INVALID.toString());
     }
@@ -56,8 +56,8 @@ public class OrderTest {
     @ParameterizedTest
     @DisplayName("메뉴가 존재하지 않을 경우 예외 발생")
     @ValueSource(strings = {"해산물파스타-2,오일파스타-1"})
-    void validateExistMenu(String menu) {
-        assertThatCode(() -> Order.from(menu))
+    void validateExistMenu(String menus) {
+        assertThatCode(() -> Order.from(menus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ORDER_INVALID.toString());
     }
@@ -65,8 +65,8 @@ public class OrderTest {
     @ParameterizedTest
     @DisplayName("메뉴가 중복될 경우 예외 발생")
     @ValueSource(strings = {"해산물파스타-2,해산물파스타-1"})
-    void validateNotDuplicated(String menu) {
-        assertThatCode(() -> Order.from(menu))
+    void validateNotDuplicated(String menus) {
+        assertThatCode(() -> Order.from(menus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ORDER_INVALID.toString());
     }
@@ -74,8 +74,8 @@ public class OrderTest {
     @ParameterizedTest
     @DisplayName("메뉴를 최대 수량 이상 시켰을 경우 예외 발생")
     @ValueSource(strings = {"해산물파스타-11,레드와인-10"})
-    void validateMaxMenuCount(String menu) {
-        assertThatCode(() -> Order.from(menu))
+    void validateMaxMenuCount(String menus) {
+        assertThatCode(() -> Order.from(menus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ORDER_MENU_COUNT_EXCEED.toString());
     }
