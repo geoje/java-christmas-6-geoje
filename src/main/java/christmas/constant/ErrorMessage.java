b8169826c2166ac7bbb7766b535dbbe3cbdc2c63
Gyeongho Yang <chchch1213@naver.com>
2023-11-11 14:35:57 +0900

    feat(const): 주문에 대한 오류 메세지 추가

--- src/main/java/christmas/constant/ErrorMessage.java
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -4,7 +4,9 @@ public enum ErrorMessage {
     EXCEPTION_PREFIX("[ERROR] "),
     DAY_NOT_NUMERIC("숫자가 아닙니다. 다시 입력해 주세요."),
     DAY_INVALID("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    ORDER_INVALID("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    ORDER_INVALID("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    ORDER_NO_HYPHEN("메뉴와 숫자를 하이픈(-) 으로 구분할 수 없습니다. 다시 입력해 주세요."),
+    ORDER_COUNT_NOT_NUMERIC("주문 개수가 숫자가 아닙니다. 다시 입력해 주세요.");
 
     private final String message;
 
