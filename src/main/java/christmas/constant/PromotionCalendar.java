2bdacda7f5c53c1219ac37bb96f3907812968b78
Gyeongho Yang <chchch1213@naver.com>
2023-11-11 22:59:05 +0900

    feat(const): 프로모션 달력에서 년수를 가져오는 메서드와 일수를 입력하고 Calendar 객체를 받는 함수 구현

--- src/main/java/christmas/constant/PromotionCalendar.java
+++ src/main/java/christmas/constant/PromotionCalendar.java
@@ -13,13 +13,24 @@ public enum PromotionCalendar {
         this.calendar = calendar;
     }
 
+    public static Calendar getPeriodCalendarWithDay(int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(PERIOD_MONTH.getYear(),
+                PERIOD_MONTH.getRealMonth() - 1,
+                day,
+                0,
+                0,
+                0);
+        return calendar;
+    }
+
     public boolean containDay(int day) {
         return calendar.getActualMinimum(Calendar.DAY_OF_MONTH) <= day &&
                 day <= calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 
-    public int getDay() {
-        return calendar.get(Calendar.DAY_OF_MONTH);
+    public int getYear() {
+        return calendar.get(Calendar.YEAR);
     }
 
     public int getRealMonth() {
