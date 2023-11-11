5e76f44f142030124f28da28f5755ee909f116e0
Gyeongho Yang <chchch1213@naver.com>
2023-11-11 14:38:06 +0900

    feat(domain): 주문 내역에 대한 Map을 자료형으로 하는 일급 컬렉션 도메인 Order Class 생성

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -2,16 +2,63 @@ package christmas.domain;
 
 import christmas.constant.Menu;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static christmas.constant.ErrorMessage.*;
+import static christmas.constant.ReceiptMessage.CONTENT_ORDER_MENU;
 
 public class Order {
-    private List<Menu> menus;
+    private static String MENU_ENTRY_DELIMITER = ",";
+    private static String MENU_NAME_COUNT_DELIMITER = "-";
+    private Map<String, Integer> menus;
 
-    public Order(List<Menu> menus) {
+    public Order(Map<String, Integer> menus) {
         this.menus = menus;
     }
 
     public static Order from(String menus) {
-        return new Order(List.of());
+        return new Order(Arrays.stream(menus.split(MENU_ENTRY_DELIMITER))
+                .filter(menu -> !menu.isBlank())
+                .map(Order::parseNameAndCount)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+    }
+
+    private static Map.Entry<String, Integer> parseNameAndCount(String menu) {
+        validateNameCountDelimiter(menu);
+        int indexDelimiter = menu.lastIndexOf(MENU_NAME_COUNT_DELIMITER);
+        String name = menu.substring(0, indexDelimiter);
+        String count = menu.substring(indexDelimiter + 1);
+
+        validateNumeric(count);
+        validateExistMenu(name);
+        return Map.entry(name, Integer.parseInt(count));
+    }
+
+    private static void validateNameCountDelimiter(String menu) {
+        if (!menu.contains(MENU_NAME_COUNT_DELIMITER)) {
+            throw new IllegalArgumentException(ORDER_NO_HYPHEN.toString());
+        }
+    }
+
+    private static void validateNumeric(String count) {
+        if (!Pattern.compile("-?\\d+").matcher(count).matches()) {
+            throw new IllegalArgumentException(ORDER_COUNT_NOT_NUMERIC.toString());
+        }
+    }
+
+    private static void validateExistMenu(String name) {
+        if (Arrays.stream(Menu.values()).noneMatch(menu -> menu.getName().equals(name))) {
+            throw new IllegalArgumentException(ORDER_INVALID.toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return menus.entrySet().stream()
+                .map(entry -> String.format(CONTENT_ORDER_MENU.toString(), entry.getKey(), entry.getValue()))
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 }
